ec7344a74f4681f7f4d100f8762006d80da14a52
boopitis <stephenravelo31@gmail.com>
2025-01-29 12:37:40 -0700

    Examples 2.3, 2.5

---
+++ ex2.3.py
@@ -0,0 +1,16 @@
+import json
+
+if __name__ == '__main__':
+    # load json data
+    with open('large-file.json', mode='r', encoding='utf-8') as in_file:
+        file_contents = in_file.read()
+    json_object = json.loads(file_contents)
+    
+    # change size field to 42
+    for data in json_object:
+        data['payload']['size'] = 42
+
+    # reverse json and write to output file
+    reversed_json_object = list(reversed(json_object))
+    with open('output.2.3.json', 'w', encoding='utf-8') as out_file:
+        json.dump(reversed_json_object, out_file, ensure_ascii=False, indent=2)
---
+++ ex2.5.py
@@ -0,0 +1,23 @@
+import json
+import timeit
+
+def change_size(data, num):
+    for data in json_object:
+        data['payload']['size'] = num
+
+if __name__ == '__main__':
+    # load json data
+    with open('large-file.json', mode='r', encoding='utf-8') as in_file:
+        file_contents = in_file.read()
+    json_object = json.loads(file_contents)
+    
+    # change size field to 42
+    elapsed_time = timeit.timeit(lambda: change_size(json_object, 42), number=10)
+
+    # reverse json and write to output file
+    reversed_json_object = list(reversed(json_object))
+    with open('output.2.3.json', 'w', encoding='utf-8') as out_file:
+        json.dump(reversed_json_object, out_file, ensure_ascii=False, indent=2)
+
+    avg_time = elapsed_time / 10
+    print("Average time to modify size in each record:", avg_time)
