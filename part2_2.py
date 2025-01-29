6f9bf801871ee17ff0e46fd120583fc5e4572f98
Tengof <vincent.fong@ucalgary.ca>
2025-01-29 12:47:53 -0700

    Part 2.2 and Part 2.4
    Questions 2.2 and 2.4

---
+++ part2_2.py
@@ -0,0 +1,38 @@
+import timeit
+
+def countVowel(content):
+    vowels = "aeiouy"
+    index = 0
+    numVowel = 0
+    numWord = 0
+    for i, l in enumerate(content):
+        if "CHAPTER 1. Loomings." in l:
+            index = i
+            
+            
+    for ind, line in enumerate(content):
+        if(ind >= index):
+            lower = line.lower()
+            numVowel += sum(lower.count(vowel) for vowel in vowels);
+            numWord += len(content[ind].split())
+
+    avg = numVowel / numWord
+    return avg
+
+def printTxt(average):
+    print("There are an average number of", average, "vowels per word in the text.")
+
+contentArr = []
+with open("pg2701.txt") as file:
+    contentArr = file.readlines()
+
+avgVowel = countVowel(contentArr)
+printTxt(avgVowel)
+
+    
+
+    
+    
+
+
+
