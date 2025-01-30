9bf4bad659f9014bbcbf2112459acd6eaea6a5e9
AaronL8 <aaron.lauang1@ucalgary.ca>
2025-01-29 17:17:34 -0700

    Update ex2.6.py

--- ex2.6.py
+++ ex2.6.py
@@ -1,24 +1,32 @@
 import timeit
 
+# calculates n-th power of 2
 def pow2_1(n):
     power = 2**n
     return power
 
+# for loop version
 def pow2_2(n):
-    if n == 0:
-        return 1
-    elif n == 1:
-        return 2
-    power = 2
-    for i in range(n - 1):
-        power *= 2
-    return power
+    pwrs_of_two = []
+    for i in range(1, n + 1):
+        pwrs_of_two.append(pow2_1(i))
+    return pwrs_of_two
 
+# list comprehension version
 def pow2_3(n):
-    pass
+    pwrs_of_two = [pow2_1(i) for i in range(1, n + 1)]
+    return pwrs_of_two
 
+# total time to calculate pow2_1(10000) 10000 times
 elapsed_time = timeit.timeit(lambda : pow2_1(10000), number=10000)
-print(f"Elapsed time of first pow2 function: {elapsed_time} seconds")
+print(f"Elapsed time of pow2_1 function: {elapsed_time} seconds")
 
+# total time to calculate and store powers of 2 up to the 1000th power in an array
+# 1000 times using a for loop
 elapsed_time = timeit.timeit(lambda : pow2_2(1000), number=1000)
-print(f"Elapsed time of pow2 function using a for loop: {elapsed_time} seconds")
+print(f"Elapsed time of pow2_2 function (for loop): {elapsed_time} seconds")
+
+# total time to calculate and store powers of 2 up to the 1000th power in an array
+# 1000 times using list comprehension
+elapsed_time = timeit.timeit(lambda : pow2_3(1000), number=1000)
+print(f"Elapsed time of pow2_3 function (list comprehension): {elapsed_time} seconds")
